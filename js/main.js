8202a67afa41e1a102eb13a3dbc8aa5e50b67a2c
namnhi-dev <namnhi.mit@gmail.com>
2025-04-02 09:11:16 +0700

    update render

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
 const API_KEY = "AIzaSyBRvqJLeuk4IRinZ1JRHfjdZsPEgF_p9b0";
-
+let countEnter = 0;
 async function getRecommendation() {
-  const keyword = document.getElementById("keyword").value;
-  const resultDiv = document.getElementById("result");
-  const loader = document.getElementById("loader");
+  const keyword = document.querySelector("#keyword").value;
+  const resultDiv = document.querySelector("#result");
+  const loader = document.querySelector("#loader");
+  const loaderIcon = document.querySelector("#loader-icon");
+  const button = document.querySelector("button");
 
+  const dataLoading = [
+    "Chill đi bạn, loading xíu thôi… 😎",
+    "Đang tải, khum lâu đâu 🌝",
+    "Chờ tầm 3 nốt nhạc… 🎶",
+    "Đợi tý, team mình đang thả thính dữ liệu… 💘",
+    "Team mình đang tranh luận xem nên hiển thị gì… 🤼",
+  ];
   if (!keyword) {
     alert("Vui lòng nhập từ khoá");
     return;
   }
 
   loader.style.display = "block";
+  loaderIcon.style.display = "block";
+  loader.innerText = dataLoading[Math.floor(Math.random() * 5)];
   resultDiv.textContent = "";
+  if (countEnter >= 5) {
+    loader.innerText = "Bình tĩnh bạn ơi, app chưa kịp thở đây nè 🌬️";
+  }
+  countEnter += 1;
 
   try {
+    button.disabled = true;
     const response = await fetch(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
       {
@@ -34,7 +50,7 @@ async function getRecommendation() {
         }),
       }
     );
-
+    countEnter >= 6 ? (countEnter = 0) : countEnter;
     const data = await response.json();
     const recommendation =
       data.candidates?.[0]?.content?.parts?.[0]?.text ||
@@ -45,5 +61,7 @@ async function getRecommendation() {
     resultDiv.textContent = "Có lỗi xảy ra, vui lòng thử lại";
   } finally {
     loader.style.display = "none";
+    loaderIcon.style.display = "none";
+    button.disabled = false;
   }
 }
